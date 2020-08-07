9d87d1283756bc290a712f3fcdef21d3b0704684
slavasdakov <68895901+slavasdakov@users.noreply.github.com>
2020-08-07 10:02:39 +0300

    Initial commit

---
+++ resources/views/menu.php
@@ -0,0 +1,3 @@
+<a href="/">Главная</a>
+<a href="/about">О нас</a>
+<a href = "/news"> Новости</a> <br>
---
+++ resources/views/news.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php include "menu.php" ?>
+НОВОСТИ<br>
+<a href="/new">Новость </a>
+</body>
+</html>
+
